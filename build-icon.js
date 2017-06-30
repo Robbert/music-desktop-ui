1e3edb3b9b5bef4d8da97dc46b7e6c6554db961a
Robbert Broersma <mail@robbertbroersma.nl>
2017-06-30 15:02:53 +0200

    Move generated icon to dist/

--- build-icon.js
+++ build-icon.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const fontkit = require('fontkit');
 const font = fontkit.openSync('/System/Library/Fonts/Apple Color Emoji.ttc').fonts[0];
 
-const outputPath = 'img/icon.png';
+const outputPath = 'dist/img/icon.png';
 
 let run = font.layout('🎵');
 let glyph = run.glyphs[0].getImageForSize(10)
