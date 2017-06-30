c09cf34040d4ba08072db785df4652d1da4cc491
Robbert Broersma <mail@robbertbroersma.nl>
2017-06-30 13:51:46 +0200

    Use fontkit to generate emoji PNG icon

---
+++ build-icon.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const fontkit = require('fontkit');
+const font = fontkit.openSync('/System/Library/Fonts/Apple Color Emoji.ttc').fonts[0];
+
+const outputPath = 'img/icon.png';
+
+let run = font.layout('🎵');
+let glyph = run.glyphs[0].getImageForSize(128)
+
+fs.writeFile(outputPath, glyph.data);
